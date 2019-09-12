4717636bdc25b5b1b02d659f78e924caf8480b6c
Andrew Hearin <ahearin@anl.gov>
2019-09-12 17:38:53 -0500

    Added python wrapper around cython module and built the extension into setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 
 
 PACKAGENAME = "galsampler"
 VERSION = "0.0.dev"
 
+
+ext0 = Extension(
+        name="galsampler.cython_kernels.galaxy_selection_kernel",
+        sources=["galsampler/cython_kernels/galaxy_selection_kernel.pyx"],
+        include_dirs=['numpy'])
+
+ext_modules = [ext0]
+cmdclass = dict(build_ext=build_ext)
 
 setup(
     name=PACKAGENAME,
@@ -15,5 +26,7 @@ setup(
     long_description="Tools for generating synthetic cosmological data",
     install_requires=["numpy", "cython"],
     packages=find_packages(),
-    url="https://github.com/LSSTDESC/galsampler"
+    url="https://github.com/LSSTDESC/galsampler",
+    cmdclass=cmdclass,
+    ext_modules=ext_modules
 )

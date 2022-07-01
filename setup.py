167ca0dfe7ad008b823ae639ae1e5ba31a57f5d7
Andrew Hearin <ahearin@anl.gov>
2022-07-01 14:20:57 -0500

    Auto detect install_requires kwarg of setup function from requirements.txt

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import find_packages
 from distutils.core import setup
 from distutils.extension import Extension
@@ -7,11 +8,16 @@ from Cython.Distutils import build_ext
 PACKAGENAME = "galsampler"
 VERSION = "0.0.dev"
 
+_THIS_DRNAME = os.path.dirname(os.path.abspath(__file__))
+REQUIREMENTS_FN = os.path.join(_THIS_DRNAME, "requirements.txt")
+with open(REQUIREMENTS_FN, "r") as f:
+    DEPENDENCIES = [req.strip() for req in f]
 
 ext0 = Extension(
-        name="galsampler.cython_kernels.galaxy_selection_kernel",
-        sources=["galsampler/cython_kernels/galaxy_selection_kernel.pyx"],
-        include_dirs=['numpy'])
+    name="galsampler.cython_kernels.galaxy_selection_kernel",
+    sources=["galsampler/cython_kernels/galaxy_selection_kernel.pyx"],
+    include_dirs=["numpy"],
+)
 
 ext_modules = [ext0]
 cmdclass = dict(build_ext=build_ext)
@@ -24,9 +30,9 @@ setup(
     author_email="ahearin@anl.gov",
     description="Tools for generating synthetic cosmological data",
     long_description="Tools for generating synthetic cosmological data",
-    install_requires=["numpy", "cython"],
+    install_requires=DEPENDENCIES,
     packages=find_packages(),
     url="https://github.com/LSSTDESC/galsampler",
     cmdclass=cmdclass,
-    ext_modules=ext_modules
+    ext_modules=ext_modules,
 )

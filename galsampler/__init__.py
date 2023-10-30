f8acb7f8614117e5f4cb46d681cc26f861c413cc
Andrew Hearin <ahearin@anl.gov>
2023-10-30 16:17:36 -0500

    Repackage based on pyproject.toml

--- galsampler/__init__.py
+++ galsampler/__init__.py
@@ -1,2 +1,6 @@
+"""
+"""
+# flake8: noqa
+from ._version import __version__
 from .crossmatch import *
 from .galmatch import *
--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-import os
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-__version__ = None
-pth = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)), "galsampler", "_version.py"
-)
-with open(pth, "r") as fp:
-    exec(fp.read())
-
-
-setup(
-    name="galsampler",
-    version=__version__,
-    author="Andrew Hearin",
-    author_email="ahearin@anl.gov",
-    description="Tools for generating synthetic cosmological data",
-    long_description="Tools for generating synthetic cosmological data",
-    install_requires=["numpy", "numba", "scipy"],
-    packages=find_packages(),
-    url="https://github.com/LSSTDESC/galsampler",
-)
+setup()
